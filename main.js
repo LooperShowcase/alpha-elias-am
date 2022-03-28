1dd14bf5db9d91f21c7279e2101ddfc69dc7ffcd
Looper Project Showcases <87170787+LooperShowcase@users.noreply.github.com>
2022-03-28 14:56:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function darkmode() {
+  document.body.style.backgroundColor = "#191919";
+}
+
+function lightmode() {
+  document.body.style.backgroundColor = "white";
+}
+function normalmode() {
+  document.body.style.backgroundColor = "pink";
+}
